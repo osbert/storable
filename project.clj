3a11f27e784d506929bc4eb8c5041c483ed68634
Osbert Feng <osbert@iterinc.com>
2015-02-27 14:38:53 -0800

    Bump underlying dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject storable "0.1.0-SNAPSHOT"
+(defproject storable "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [environ "0.4.0"]
-                 [com.datomic/datomic-pro "0.9.4755" :exclusions [org.apache.httpcomponents/httpclient]]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [environ "1.0.0"]
+                 [com.datomic/datomic-pro "0.9.5130" :exclusions [org.apache.httpcomponents/httpclient]]]
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :username [:gpg  :env/MY_DATOMIC_USERNAME]
                                    :password [:gpg :env/MY_DATOMIC_PASSWORD]}}
-  :plugins [[lein-environ "0.5.0"]])
+  :plugins [[lein-environ "1.0.0"]])
+
