306c4e80d2dbf52d4c0bf2395da11e019e46ccf6
Osbert Feng <osbert@iterinc.com>
2015-03-03 15:48:10 -0800

    Change default dependencies to be datomic-free in dev, datomic-pro in production/uberjar.

--- project.clj
+++ project.clj
@@ -4,10 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [environ "1.0.0"]
-                 [com.datomic/datomic-pro "0.9.5130" :exclusions [org.apache.httpcomponents/httpclient]]]
+                 [environ "1.0.0"]]
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :username [:gpg  :env/MY_DATOMIC_USERNAME]
                                    :password [:gpg :env/MY_DATOMIC_PASSWORD]}}
-  :plugins [[lein-environ "1.0.0"]])
+  :plugins [[lein-environ "1.0.0"]]
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5130" :exclusions [joda-time org.apache.httpcomponents/httpclient]]]}
+             :production {:dependencies [[com.datomic/datomic-pro "0.9.5130" :exclusions [joda-time org.apache.httpcomponents/httpclient]]]}
+             :uberjar {:dependencies [[com.datomic/datomic-pro "0.9.5130" :exclusions [joda-time org.apache.httpcomponents/httpclient]]]}})
 
