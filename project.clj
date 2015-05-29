c9f29562f631cd0ed0ceb41263e9001aebe558fb
Osbert Feng <osbert@iterinc.com>
2015-05-28 17:02:11 -0700

    0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.iterinc/storable "0.5.1"
+(defproject com.iterinc/storable "0.5.2"
   :description "Utility functions for basic Clojure maps <=> Datomic"
   :url "https://github.com/osbert/storable"
   :license {:name "Eclipse Public License"
